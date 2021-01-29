a3b856610c5eb2b0ce51b22ffe42e7dc1822d823
Victor Navarro <vn4varro@gmail.com>
2021-01-29 19:42:15 +0100

    feat: mvp of concept idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react';
+import {toPng} from 'html-to-image'
+import download from 'downloadjs'
+import kittenImage from './kitten.jpg'
 
-function App() {
+import './App.css'
+
+const App = () => {
+  const ref = useRef<HTMLDivElement>(null)
+
+  const handleClick = async () => {
+    if (!ref.current) {
+      return
+    }
+
+    const images = Array.from(ref.current.childNodes)
+    let index = 1;
+
+    for (const image of images) {
+      const dataUrl = await toPng(image as HTMLElement)
+      download(dataUrl, `file-${index}.png`)
+      index += 1;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <button onClick={handleClick}>Download</button>
+      <div style={{height: 1000}}></div>
+      <div ref={ref}>
+        {[1, 2, 3, 4].map(number => {
+          return (
+            <div key={number} className="input-container">
+              <img height={200} src={kittenImage}/>
+              <p>{number}</p>
+            </div>
+          )
+        })}
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
